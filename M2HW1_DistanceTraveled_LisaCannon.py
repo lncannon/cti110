8b798eead9668a7573b2c7758dde3c392329e6b2
lncannon <31830717+lncannon@users.noreply.github.com>
2017-09-10 19:12:26 -0400

    Add files via upload
    Distance Calculation 10SEP17

---
+++ M2HW1_DistanceTraveled_LisaCannon.py
@@ -0,0 +1,24 @@
+# CTI-110 
+# M2HW1 - Distance Traveled  
+# Lisa Cannon
+# 10SEP17
+# Calculating the distance traveled by a car.
+speed = '70'
+distanceAfter6 = 70 * 6
+distanceAfter10 = 70 * 10
+distanceAfter15 = 70 * 15
+
+# Distance after 6 hours at 70mph.
+print("The distance traveled after 6 hours at 70 mph is", distanceAfter6)
+
+
+# Distance after 10 hours at 70mph.
+print("The distance traveled after 10 hours at 70 mph is", distanceAfter10)
+
+# Distance after 15 hours at 70mph.
+print("The distance traveled after 15 hours at 70 mph is", distanceAfter15)
+
+
+# I do not feel that I met the objective of this assignment.
+# I would think that I should be calculating a formula such as 'distanceAfter6 = speed * 6',
+# but when I do that I get six 70's in a row. EPIC FAIL!
