bdf4a24be2e61190129b63c1ed16df80ddef292f
lncannon <31830717+lncannon@users.noreply.github.com>
2017-09-10 20:19:02 -0400

    Add files via upload
    Correct version of assignment

--- M2HW1_DistanceTraveled_LisaCannon.py
+++ M2HW1_DistanceTraveled_LisaCannon.py
@@ -3,10 +3,10 @@
 # Lisa Cannon
 # 10SEP17
 # Calculating the distance traveled by a car.
-speed = '70'
-distanceAfter6 = 70 * 6
-distanceAfter10 = 70 * 10
-distanceAfter15 = 70 * 15
+speed = int('70')
+distanceAfter6 = speed * 6
+distanceAfter10 = speed * 10
+distanceAfter15 = speed * 15
 
 # Distance after 6 hours at 70mph.
 print("The distance traveled after 6 hours at 70 mph is", distanceAfter6)
@@ -19,6 +19,3 @@ print("The distance traveled after 10 hours at 70 mph is", distanceAfter10)
 print("The distance traveled after 15 hours at 70 mph is", distanceAfter15)
 
 
-# I do not feel that I met the objective of this assignment.
-# I would think that I should be calculating a formula such as 'distanceAfter6 = speed * 6',
-# but when I do that I get six 70's in a row. EPIC FAIL!
